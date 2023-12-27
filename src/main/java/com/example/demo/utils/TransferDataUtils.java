30f8d54771da00e7e80a579c7ca005ac3398b3da
ihor <work.bibi4kov@gmail.com>
2023-12-27 21:39:32 +0200

    trivial changeg to TransferDataUtils.java

--- src/main/java/com/example/demo/utils/TransferDataUtils.java
+++ src/main/java/com/example/demo/utils/TransferDataUtils.java
@@ -3,6 +3,8 @@ package com.example.demo.utils;
 import com.example.demo.entities.User;
 import com.example.demo.dto.UserDto;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class TransferDataUtils {
@@ -14,4 +16,16 @@ public class TransferDataUtils {
     public static UserDto userEntityToDto(User user) {
         return new UserDto(user.getId().toString(), user.getName(), user.getEmail());
     }
+
+
+    private interface Person{
+        record MyPerson(int id, String name, String email, int age){}
+        Map<Integer, MyPerson> personsData = Map.of(
+                15, new MyPerson(15, "Ihor", "dfgdh4@email.com", 34),
+                18, new MyPerson(18, "David", "dsfsdf3@email.com", 24));
+        static void launch(){
+            Map<Integer, MyPerson> map = new HashMap<>(personsData);
+        }
+    }
+
 }
